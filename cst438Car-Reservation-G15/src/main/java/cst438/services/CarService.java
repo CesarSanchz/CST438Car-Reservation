5c8213f9690b079d0add48e97312353418943f8a
Cesar <sanchzcesr@gmail.com>
2021-06-16 17:31:56 -0700

    Updated service

--- cst438Car-Reservation-G15/src/main/java/cst438/services/CarService.java
+++ cst438Car-Reservation-G15/src/main/java/cst438/services/CarService.java
@@ -1,16 +1,10 @@
 package cst438.services;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import cst438.domain.*;
 import cst438.repositories.CarRepository;
@@ -18,8 +12,6 @@ import cst438.repositories.ReservationRepository;
 
 @Service
 public class CarService {
-	
-	private static final Logger log = LoggerFactory.getLogger(CarService.class);
 	
 	@Autowired
 	private CarRepository carRepository;
@@ -95,32 +87,6 @@ public class CarService {
 	public List<Reservation> findEmails(String email) {
 		// TODO Auto-generated method stub
 		return reservationRepository.findEmails(email);
-	}
-	
-	/*
-	 public Reservation makeReservation(int id, String email) {
-		    Car car = carRepository.findById(id);
-		    Seat seat = seatRepository.findBySeatId(seatId);
-
-		    // Check if any of the entered IDs are invalid
-		    if (user == null || flight == null || seat == null) {
-		      return null;
-		    }
-
-		    // Set seat to unavailable
-		    seatRepository.setSeatToUnavailable(seatId);
-
-		    Reservation reservation = new Reservation(user, passengerFirstName, passengerLastName, flight,
-		        seat, flight.getPrice());
-
-		    reservationRepository.save(reservation);
-
-		    Reservation reservationFromDb =
-		        reservationRepository.findByReservationId(reservation.getReservationId());
-
-		    return reservationFromDb;
-		  }
-	 */
-		
+	}		
 	
 }
